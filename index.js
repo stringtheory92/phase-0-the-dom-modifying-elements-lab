cd569dbebda24910aed916be8099de606dee3389
Adam Kabak <adamkabak@yahoo.com>
2022-09-21 03:56:53 -0400

    commit tree 91815d125190be035cc26076e7ef9a9d435bfe89

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+const main = document.querySelector('#main');
+main.remove();
+
+const newHeader = document.body.querySelector('#victory');
+//newHeader.id = 'victory';
+
+//const script = document.getElementsByTagName('script');
+//document.body.prepend(newHeader);
+//body.before(newHeader);
+
+//newHeader.textContent = "YOUR-NAME is the champion";
